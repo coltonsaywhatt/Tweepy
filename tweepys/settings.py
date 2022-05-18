2c2368e218e17259cffe35b2ad7bf0212dfef38e
coltonsaywhatt <coltonsaywhatt@gmail.com>
2022-05-18 16:57:58 -0400

    Config deployment

--- tweepys/settings.py
+++ tweepys/settings.py
@@ -135,3 +135,7 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
+
